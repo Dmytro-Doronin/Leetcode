4c8ee60196966dd88f42c7ea6e254305e15798cd
Dmytro-Doronin <doronindmytro623@gmail.com>
2023-12-25 22:56:13 +0100

    new

--- Easy/Test.js
+++ Easy/Test.js
@@ -1,22 +1,33 @@
-console.log(1)
+// console.log(1)
+//
+// setTimeout(() => {
+//     console.log(2)
+// }, 1000)
+//
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((res) => res.json())
+//     .then(res => console.log(3))
+//
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((res) => res.json())
+//     .then(res => console.log(4))
+//
+// setTimeout(() => {
+//     console.log(5)
+//     setTimeout(() => {
+//         console.log(6)
+//     }, 1000)
+// }, 1000)
+//
+// console.log(7)
 
-setTimeout(() => {
-    console.log(2)
-}, 1000)
+const data1 = new Date().toISOString()
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    .then(res => console.log(3))
+const currentDate = new Date();
+const tomorrowDate = new Date(currentDate);
+tomorrowDate.setDate(currentDate.getDate() + 1);
+const isoStringWithAddedDay = tomorrowDate.toISOString();
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    .then(res => console.log(4))
-
-setTimeout(() => {
-    console.log(5)
-    setTimeout(() => {
-        console.log(6)
-    }, 1000)
-}, 1000)
-
-console.log(7)
+console.log(isoStringWithAddedDay);
+console.log(data1);
+console.log(+new Date() * 1000)
